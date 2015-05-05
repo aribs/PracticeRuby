6d3725b13f89b9810323fbb68244a9ae2afb22ba
Alejo Ribes <alejoribes@gmail.com>
2015-05-05 18:06:59 +0200

    Repairing errors

--- accounts.rb
+++ accounts.rb
@@ -1,28 +1,28 @@
 class Account
 
-	def initialize user, pasword, website
-		@user = user
-		@pasword = pasword
-		@website = website
-	end
-	def view_account
-		puts "User: #{@user}, pasword: #{@pasword}, website: #{@website} "
-	end
-	def encrypt_pasword
-		encrypted_p = ""
-		for i in 0..@pasword.length - 1
-			encrypted_p << @pasword[i].ord + 3
-		end
-		@encrypted = encrypted_p
-		return encrypted_p
-	end
-	def decrypt_pasword
-		decrypted_p = ""
-		for i in 0..@encrypted.length - 1
-			decrypted_p << @encrypted[i].ord - 3
-		end
-		return decrypted_p
-	end
+    def initialize user, pasword, website
+        @user = user
+        @pasword = pasword
+        @website = website
+    end
+    def view_account
+        puts "User: #{@user}, pasword: #{@pasword}, website: #{@website} "
+    end
+    def encrypt_pasword
+        encrypted_p = ""
+        for i in 0..@pasword.length - 1
+            encrypted_p << @pasword[i].ord + 3
+        end
+        @encrypted = encrypted_p
+        return encrypted_p
+    end
+    def decrypt_pasword
+        decrypted_p = ""
+        for i in 0..@encrypted.length - 1
+            decrypted_p << @encrypted[i].ord - 3
+        end
+        return decrypted_p
+    end
 
 
 end
@@ -33,3 +33,5 @@ puts myFacebook.view_account
 puts google.view_account
 puts myFacebook.encrypt_pasword
 puts myFacebook.decrypt_pasword
+puts google.encrypt_pasword
+puts google.decrypt_pasword
