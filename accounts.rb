52506b701b5681db76e46205923d77a7bb74eb20
Alejo Ribes <alejoribes@gmail.com>
2015-05-06 09:37:37 +0200

    Added limits of caracter

--- accounts.rb
+++ accounts.rb
@@ -1,17 +1,24 @@
 class Account
-
+    attr_accessor 
     def initialize user, pasword, website
         @user = user
         @pasword = pasword
-        @website = website
+        @website = website 
+        encrypt_pasword
+
     end
     def view_account
-        puts "User: #{@user}, pasword: #{@pasword}, website: #{@website} "
+        puts "User: #{@user}, pasword: #{@encrypted}, website: #{@website} "
     end
     def encrypt_pasword
         encrypted_p = ""
         for i in 0..@pasword.length - 1
-            encrypted_p << @pasword[i].ord + 3
+            if @pasword[i].ord <= 120
+                encrypted_p << @pasword[i].ord + 3
+            else
+                encrypted_p << @pasword[i].ord 
+            end
+
         end
         @encrypted = encrypted_p
         return encrypted_p
@@ -19,7 +26,11 @@ class Account
     def decrypt_pasword
         decrypted_p = ""
         for i in 0..@encrypted.length - 1
-            decrypted_p << @encrypted[i].ord - 3
+            if @pasword[i].ord <= 120
+                decrypted_p << @encrypted[i].ord - 3
+            else
+                decrypted_p << @encrypted[i].ord 
+            end
         end
         return decrypted_p
     end
@@ -31,7 +42,4 @@ google = Account.new "userAlejo", "mypasword", "google.com"
 
 puts myFacebook.view_account
 puts google.view_account
-puts myFacebook.encrypt_pasword
-puts myFacebook.decrypt_pasword
-puts google.encrypt_pasword
 puts google.decrypt_pasword
